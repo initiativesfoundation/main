5f5dd50f1dd7d045ec8804198d6924ea46d47a66
strfayaz <initiativesfoundation@gmail.com>
2021-11-10 13:12:43 +0600

    t

---
+++ registration/forms.js
@@ -0,0 +1,56 @@
+function submission() {
+	
+	var e2 = document.getElementById("forms");
+ 	e2.classList.remove("hidden");
+ 	document.getElementById('forms').src = "https://docs.google.com/forms/d/e/1FAIpQLScUt-noRwMRKaIVF0fX3YbEUCzSaC2BFd5x8ijOKDgIybC8Kw/viewform?embedded=true";
+
+}
+
+function anime() {
+
+	var e2 = document.getElementById("forms");
+ 	e2.classList.remove("hidden");
+ 	document.getElementById('forms').src = "https://docs.google.com/forms/d/e/1FAIpQLScm1m2A2_hoBzi3MVvak4jRcd4_Vd3mp-jDT0BCL9IMSIvoHA/viewform?embedded=true";
+}
+
+function netflix() {
+
+	var e2 = document.getElementById("forms");
+ 	e2.classList.remove("hidden");
+ 	document.getElementById('forms').src = "https://docs.google.com/forms/d/e/1FAIpQLSen_aE47Ed1ATMmInYgHJO4rjSbkjMa8csfJNCyB4kxWDy7cw/viewform?embedded=true";
+}
+
+function marvel_dc() {
+
+	var e2 = document.getElementById("forms");
+ 	e2.classList.remove("hidden");
+ 	document.getElementById('forms').src = "https://docs.google.com/forms/d/e/1FAIpQLSfuo1DOT3RcEFTQ1QYCtybFfLcH0GrcjzExP-iqm32WuD_MUg/viewform?embedded=true";
+}
+
+function pop_culture() {
+
+	var e2 = document.getElementById("forms");
+ 	e2.classList.remove("hidden");
+ 	document.getElementById('forms').src = "https://docs.google.com/forms/d/e/1FAIpQLSfHr1KfkuCuWMVU1mEz7mQ2b2KyYMTGIH-MAIv1LE6_5-lrnQ/viewform?embedded=true";
+}
+
+function google_anime() {
+
+	var e2 = document.getElementById("forms");
+ 	e2.classList.remove("hidden");
+ 	document.getElementById('forms').src = "https://docs.google.com/forms/d/e/1FAIpQLSedSk-PuOF7BGjfSWCHXR0hVZNdcT2fPo4iBqK-FjX0bYjHCA/viewform?embedded=true";
+}
+
+function marvel_dc_google() {
+
+	var e2 = document.getElementById("forms");
+ 	e2.classList.remove("hidden");
+ 	document.getElementById('forms').src = "https://docs.google.com/forms/d/e/1FAIpQLScnL3KoKAJHYZFYY_c9JxcVY_4YnQ7EprATTPeE5nIg7mIr6w/viewform?embedded=true";
+}
+
+function campus_ambassador() {
+
+	var e2 = document.getElementById("forms");
+ 	e2.classList.remove("hidden");
+ 	document.getElementById('forms').src = "https://docs.google.com/forms/d/e/1FAIpQLSeWTyrI3Fq68aqlJaxVm373B4lknOf0Q4Uzcn86iTqZZgyFyA/viewform?embedded=true";
+}
